faa83f5e37ed7fa62788bb30df696fd15784387a
ahnafabir <ahnafabir313@gmail.com>
2022-02-14 18:03:24 +0600

    Primary page set-up done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import NotFound from "./pages/404/NotFound";
+import NavigationBar from "./components/NavigationBar";
+
 function App() {
   return (
-    <div>
-      <h1>Project Started</h1>
-    </div>
+    <>
+      <NavigationBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
