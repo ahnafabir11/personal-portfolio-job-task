3d01a3c669f708a7e59791cdb9f928ca2275b595
ahnafabir <ahnafabir313@gmail.com>
2022-02-16 09:10:51 +0600

    counting category dynamically and added filter method in portfolio page

--- src/components/portfolio/PortfolioList.js
+++ src/components/portfolio/PortfolioList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import portfolio1 from '../../images/portfolio-1.png';
 import portfolio2 from '../../images/portfolio-2.png';
 import portfolio3 from '../../images/portfolio-3.png';
@@ -37,30 +37,66 @@ const workList = [
   },
   {
     id: 5,
-    tagName: "Branding",
+    tagName: "Motion",
     title: "KeyBoard",
     bgImage: portfolio2,
   },
-  
+
 ]
 
 const PortfolioList = () => {
+  const [categoryList, setCategoryList] = useState([{ tagName: "See All", count: workList.length }]);
+  const [categoryName, setCategoryName] = useState('See All');
+  const [filteredList, setFilteredList] = useState(workList);
+
+  // counting category
+  useEffect(() => {
+    const res = {};
+    workList.forEach((obj) => {
+      const key = `${obj.tagName}`;
+      if (!res[key]) {
+        res[key] = { tagName: obj.tagName, count: 0 };
+      };
+      res[key].count += 1;
+    });
+    const newWorkList = Object.values(res);
+
+    setCategoryList([...categoryList, ...newWorkList])
+  }, [])
+
+  // filter method
+  useEffect(() => {
+    if (categoryName === "See All") {
+      setFilteredList(workList);
+    } else {
+      const filteredWorkList = workList.filter(work => work.tagName === categoryName);
+      setFilteredList(filteredWorkList)
+    }
+  }, [categoryName])
+
+
   return (
     <section className="bg-[#FDF0E9]">
       <div className="container">
         {/* portfolio navigation */}
         <div className="max-w-[45%] mx-auto text-[#391400] text-sm font-medium flex justify-evenly py-10">
-          <p>Show All<sup>14</sup></p>
-          <p>Design<sup>6</sup></p>
-          <p>Branding<sup>4</sup></p>
-          <p>Illustration<sup>3</sup></p>
-          <p>Motion<sup>1</sup></p>
+          {
+            categoryList.map((category, index) =>
+              <p
+                key={index}
+                className="cursor-pointer"
+                onClick={() => setCategoryName(category.tagName)}
+              >
+                {category.tagName} <sup>{category.count}</sup>
+              </p>
+            )
+          }
         </div>
 
         {/* portfolio lists */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
           {
-            workList.map(work =>
+            filteredList.map(work =>
               <ProjectBox
                 key={work.id}
                 tagName={work.tagName}
